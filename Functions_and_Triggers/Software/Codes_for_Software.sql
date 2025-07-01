a19f350f7e05f54565428a121bb867aa4bda510a
Joseph-DD2 <catce.2024111tads0038@aluno.ifpi.edu.br>
2025-06-30 22:43:39 -0300

    Update Codes_for_Software.sql

--- Functions_and_Triggers/Software/Codes_for_Software.sql
+++ Functions_and_Triggers/Software/Codes_for_Software.sql
@@ -35,7 +35,7 @@ CREATE OR REPLACE FUNCTION atualizar_software(u_produto_id INT, u_tipo_licenca V
     DECLARE
         u_old RECORD;
     BEGIN
-        SELECT * INTO u_old  FROM "software"
+        SELECT * INTO u_old FROM "software"
         WHERE "produto_id" = u_produto_id;
         IF NOT FOUND THEN
             RAISE NOTICE 'Nenhum registro em software para o produto % encontrado.', u_produto_id;
@@ -78,112 +78,60 @@ CREATE OR REPLACE FUNCTION excluir_software(d_produto_id INT, d_tipo_licenca VAR
 $$ LANGUAGE plpgsql;
 
 -- Listar softwares
-CREATE OR REPLACE FUNCTION listar_softwares()
-RETURNS TABLE (
-    id INT,
-    nome VARCHAR,
-    descricao TEXT,
-    preco NUMERIC,
-    status STATUS_PRODUTOS,
-    tipo_licenca VARCHAR,
-    data_publicacao DATE
-) AS $$
-BEGIN
-    RETURN QUERY
-    SELECT
-        p.id_produto,
-        p.nome_produto,
-        p.descricao,
-        p.preco,
-        p.status,
-        s.tipo_licenca,
-        p.data_publicacao
-    FROM produto p
-    JOIN software s ON s.produto_id = p.id_produto;
-END;
+CREATE OR REPLACE FUNCTION listar_softwares() RETURNS TABLE (id INT, nome VARCHAR, descricao TEXT, preco NUMERIC, status STATUS_PRODUTOS, tipo_licenca VARCHAR, data_publicacao DATE) AS $$
+    BEGIN
+        RETURN QUERY
+        SELECT p.id_produto, p.nome_produto, p.descricao, p.preco, p.status, s.tipo_licenca, p.data_publicacao FROM produto p
+        JOIN software s ON s.produto_id = p.id_produto;
+    END;
 $$ LANGUAGE plpgsql;
 
 -- buscar por nome
-CREATE OR REPLACE FUNCTION buscar_softwares_por_nome(p_nome TEXT)
-RETURNS TABLE (
-    id INT,
-    nome VARCHAR,
-    tipo_licenca VARCHAR,
-    status STATUS_PRODUTOS
-) AS $$
-BEGIN
-    RETURN QUERY
-    SELECT
-        p.id_produto,
-        p.nome_produto,
-        s.tipo_licenca,
-        p.status
-    FROM produto p
-    JOIN software s ON s.produto_id = p.id_produto
-    WHERE p.nome_produto ILIKE '%' || p_nome || '%';
-END;
+CREATE OR REPLACE FUNCTION buscar_softwares_por_nome(p_nome TEXT) RETURNS TABLE (id INT, nome VARCHAR, tipo_licenca VARCHAR, status STATUS_PRODUTOS) AS $$
+    BEGIN
+        RETURN QUERY
+        SELECT p.id_produto, p.nome_produto, s.tipo_licenca, p.status FROM produto p
+        JOIN software s ON s.produto_id = p.id_produto
+        WHERE p.nome_produto ILIKE '%' || p_nome || '%';
+    END;
 $$ LANGUAGE plpgsql;
 
 -- Listar por licença
-CREATE OR REPLACE FUNCTION listar_softwares_por_licenca(p_licenca TEXT)
-RETURNS TABLE (
-    id INT,
-    nome VARCHAR,
-    descricao TEXT,
-    preco NUMERIC,
-    status STATUS_PRODUTOS
-) AS $$
-BEGIN
-    RETURN QUERY
-    SELECT
-        p.id_produto,
-        p.nome_produto,
-        p.descricao,
-        p.preco,
-        p.status
-    FROM produto p
-    JOIN software s ON s.produto_id = p.id_produto
-    WHERE s.tipo_licenca ILIKE '%' || p_licenca || '%';
-END;
+CREATE OR REPLACE FUNCTION listar_softwares_por_licenca(p_licenca TEXT) RETURNS TABLE (id INT, nome VARCHAR, descricao TEXT, preco NUMERIC, status STATUS_PRODUTOS) AS $$
+    BEGIN
+        RETURN QUERY
+        SELECT p.id_produto, p.nome_produto, p.descricao, p.preco, p.status FROM produto p
+        JOIN software s ON s.produto_id = p.id_produto
+        WHERE s.tipo_licenca ILIKE '%' || p_licenca || '%';
+    END;
 $$ LANGUAGE plpgsql;
 
 -- listar ativos
-CREATE OR REPLACE FUNCTION listar_softwares_ativos()
-RETURNS TABLE (
-    id INT,
-    nome VARCHAR,
-    tipo_licenca VARCHAR
-) AS $$
-BEGIN
-    RETURN QUERY
-    SELECT
-        p.id_produto,
-        p.nome_produto,
-        s.tipo_licenca
-    FROM produto p
-    JOIN software s ON s.produto_id = p.id_produto
-    WHERE p.status = 'ativo';
-END;
+CREATE OR REPLACE FUNCTION listar_softwares_ativos() RETURNS TABLE (id INT, nome VARCHAR, tipo_licenca VARCHAR) AS $$
+    BEGIN
+        RETURN QUERY
+        SELECT p.id_produto, p.nome_produto, s.tipo_licenca FROM produto p
+        JOIN software s ON s.produto_id = p.id_produto
+        WHERE p.status = 'ativo';
+    END;
 $$ LANGUAGE plpgsql;
 
 -- Triggers
 
 -- validar
-CREATE OR REPLACE FUNCTION verificar_tipo_produto_software()
-RETURNS TRIGGER AS $$
-DECLARE
-    tipo_prod TIPOS_PRODUTOS;
-BEGIN
-    SELECT tipo INTO tipo_prod
-    FROM produto
-    WHERE id_produto = NEW.produto_id;
+CREATE OR REPLACE FUNCTION verificar_tipo_produto_software() RETURNS TRIGGER AS $$
+    DECLARE
+        tipo_prod TIPOS_PRODUTOS;
+    BEGIN
+        SELECT tipo INTO tipo_prod FROM produto
+        WHERE id_produto = NEW.produto_id;
 
-    IF tipo_prod IS DISTINCT FROM 'software' THEN
-        RAISE EXCEPTION 'O produto associado não é do tipo SOFTWARE.';
-    END IF;
+        IF tipo_prod IS DISTINCT FROM 'software' THEN
+            RAISE EXCEPTION 'O produto associado não é do tipo SOFTWARE.';
+        END IF;
 
-    RETURN NEW;
-END;
+        RETURN NEW;
+    END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER trg_verificar_tipo_software

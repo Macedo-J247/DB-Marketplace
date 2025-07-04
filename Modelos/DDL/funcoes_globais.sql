e518442baa7267027ed990cd305e2844daae7cf5
Joseph-DD2 <catce.2024111tads0038@aluno.ifpi.edu.br>
2025-07-04 20:09:43 -0300

    Update funcoes_globais.sql

--- Modelos/DDL/funcoes_globais.sql
+++ Modelos/DDL/funcoes_globais.sql
@@ -1,11 +1,23 @@
 -- Inserções globais por nome de tabela
 CREATE OR REPLACE FUNCTION insercao_global() RETURNS INT AS $$
+    DECLARE
+        resultado INT;
+    BEGIN
+    END;
 $$ LANGUAGE plpgsql;
 
 -- Atualizações globais por nome de tabela
 CREATE OR REPLACE FUNCTION atualizacao_global() RETURNS INT AS $$
+    DECLARE
+        resultado INT;
+    BEGIN
+    END;
 $$ LANGUAGE plpgsql;
 
 -- Remoções globais por nome de tabela
-CREATE OR REPLACE FUNCTION remocao_global() RETURNS INT AS $$
+CREATE OR REPLACE FUNCTION remocao_global() RETURNS TEXT AS $$
+    DECLARE
+        resultado TEXT;
+    BEGIN
+    END;
 $$ LANGUAGE plpgsql;

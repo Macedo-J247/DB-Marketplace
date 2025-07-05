abbda53729578c958c269ddada70554dc13d350d
Joseph-DD2 <catce.2024111tads0038@aluno.ifpi.edu.br>
2025-07-05 15:13:31 -0300

    Update triggers_globais.sql

--- Modelos/DDL/triggers_globais.sql
+++ Modelos/DDL/triggers_globais.sql
@@ -1,16 +1,16 @@
--- Trigger para impedir exclusão do próprio administrador
-CREATE OR REPLACE FUNCTION trigger_bloquear_exclusao_admin()
-RETURNS TRIGGER AS $$
-BEGIN
-    IF OLD.tipo_usuario = 'admin' THEN
-        RAISE EXCEPTION 'Não é permitido excluir um usuário do tipo "admin" diretamente.';
-    END IF;
-    RETURN OLD;
-END;
+-- Triggers
+
+-- Restrição de exclusão do próprio administrador
+CREATE OR REPLACE FUNCTION trigger_bloquear_exclusao_admin() RETURNS TRIGGER AS $$
+    BEGIN
+        IF OLD.tipo_usuario = 'admin' THEN
+            RAISE EXCEPTION 'Não é permitido excluir um usuário do tipo "admin" diretamente.';
+        END IF;
+        RETURN OLD;
+    END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER trg_bloquear_exclusao_admin
 BEFORE DELETE ON usuario
 FOR EACH ROW
 EXECUTE FUNCTION trigger_bloquear_exclusao_admin();
-

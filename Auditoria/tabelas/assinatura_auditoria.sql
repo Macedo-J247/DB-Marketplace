c0f5b220024db2d08beaeef2513f3adceaf86964
wadson2005 <wadsondias62@gmail.com>
2025-07-05 11:27:06 -0300

    auditoria para a tabela assinatura

---
+++ Auditoria/tabelas/assinatura_auditoria.sql
@@ -0,0 +1,12 @@
+CREATE TABLE auditoria_assinatura (
+    id_auditoria SERIAL PRIMARY KEY,
+    id_assinatura INT,
+    acao VARCHAR(10), -- 'INSERT', 'UPDATE', 'DELETE'
+    usuario_id INT,
+    versao_id INT,
+    tipo_pagamento_id INT,
+    data_inicio DATE,
+    data_termino DATE,
+    status STATUS_ASSINATURA,
+    data_evento TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);

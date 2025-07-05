0372d4f7a2fbddceb28a444c071b0ba45e1d1ea8
Joseph-DD2 <catce.2024111tads0038@aluno.ifpi.edu.br>
2025-07-05 15:45:51 -0300

    Auditorias

--- Auditoria/tabelas/auditoria.sql
+++ Auditoria/tabelas/auditoria.sql
@@ -36,3 +36,71 @@ CREATE TABLE auditoria_desenvolvedor (
     data_cadastro DATE,
     data_acao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+-- Auditoria para a tabela de produto
+CREATE TABLE auditoria_produto (
+    id_auditoria SERIAL PRIMARY KEY,
+    id_produto INT,
+    acao VARCHAR(10),
+    desenvolvedor_id INT,
+    categoria_id INT,
+    nome_produto VARCHAR(255),
+    descricao TEXT,
+    preco DECIMAL(10,2),
+    tipo TIPOS_PRODUTOS,
+    status STATUS_PRODUTOS,
+    data_publicacao DATE,
+    data_atualizacao DATE,
+    data_acao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Auditoria para a tabela de versão
+CREATE TABLE auditoria_versao (
+    id_auditoria SERIAL PRIMARY KEY,
+    id_versao INT,
+    acao VARCHAR(10),
+    produto_id INT,
+    num_versao VARCHAR(50),
+    data_lancamento DATE,
+    data_acao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Auditoria para a tabela de suporte
+CREATE TABLE auditoria_suporte (
+    id_auditoria SERIAL PRIMARY KEY,
+    id_suporte INT,
+    acao VARCHAR(10),
+    usuario_id INT,
+    produto_id INT,
+    versao_id INT,
+    tipo TIPOS_SUPORTES,
+    descricao TEXT,
+    data_suporte TIMESTAMP,
+    status STATUS_SUPORTE,
+    data_acao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Auditoria para a tabela de avaliação
+CREATE TABLE auditoria_avaliacao (
+    id_auditoria SERIAL PRIMARY KEY,
+    id_avaliacao INT,
+    acao VARCHAR(10),
+    usuario_id INT,
+    versao_id INT,
+    nota DECIMAL(3,2),
+    data_avaliacao DATE,
+    data_acao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Auditoria para a tabela de parcela
+CREATE TABLE auditoria_parcela (
+    id_auditoria SERIAL PRIMARY KEY,
+    id_parcela INT,
+    acao VARCHAR(10),
+    assinatura_id INT,
+    valor DECIMAL(10,2),
+    data_vencimento DATE,
+    data_pagamento DATE,
+    status STATUS_PARCELA,
+    data_acao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
--- Auditoria/triggers_and_functions_auditoria/codes_for_auditoria.sql
+++ Auditoria/triggers_and_functions_auditoria/codes_for_auditoria.sql
@@ -1,6 +1,6 @@
 -- Triggers
 
--- Trigger para preenchimento da tabela de auditoria referente a tabela assinatura
+-- Trigger para preenchimento da tabela de auditoria referente à tabela assinatura
 CREATE OR REPLACE FUNCTION log_auditoria_assinatura() RETURNS TRIGGER AS $$
     BEGIN
         IF (TG_OP = 'INSERT') THEN
@@ -30,7 +30,7 @@ AFTER INSERT OR UPDATE OR DELETE ON assinatura
 FOR EACH ROW
 EXECUTE FUNCTION log_auditoria_assinatura();
 
--- Trigger para preenchimento da tabela de auditoria referente a tabela assinatura
+-- Trigger para preenchimento da tabela de auditoria referente à tabela assinatura
 CREATE OR REPLACE FUNCTION log_auditoria_usuario() RETURNS TRIGGER AS $$
     BEGIN
         IF (TG_OP = 'INSERT') THEN
@@ -55,7 +55,7 @@ AFTER INSERT OR UPDATE OR DELETE ON usuario
 FOR EACH ROW
 EXECUTE FUNCTION log_auditoria_usuario();
 
--- Trigger para preenchimento da tabela de auditoria referente a tabela desenvolvimento
+-- Trigger para preenchimento da tabela de auditoria referente à tabela desenvolvimento
 CREATE OR REPLACE FUNCTION log_auditoria_desenvolvedor() RETURNS TRIGGER AS $$
 BEGIN
     IF (TG_OP = 'INSERT') THEN
@@ -79,3 +79,128 @@ CREATE TRIGGER trg_auditoria_desenvolvedor
 AFTER INSERT OR UPDATE OR DELETE ON desenvolvedor
 FOR EACH ROW
 EXECUTE FUNCTION log_auditoria_desenvolvedor();
+
+-- Trigger para preenchimento da tabela de auditoria referente à tabela produto
+CREATE OR REPLACE FUNCTION log_auditoria_produto() RETURNS TRIGGER AS $$
+BEGIN
+    IF (TG_OP = 'INSERT') THEN
+        INSERT INTO auditoria_produto(id_produto, acao, desenvolvedor_id, categoria_id, nome_produto, descricao, preco, tipo, status, data_publicacao, data_atualizacao)
+        VALUES (NEW.id_produto, 'INSERT', NEW.desenvolvedor_id, NEW.categoria_id, NEW.nome_produto, NEW.descricao, NEW.preco, NEW.tipo, NEW.status, NEW.data_publicacao, NEW.data_atualizacao);
+        RETURN NEW;
+    ELSIF (TG_OP = 'UPDATE') THEN
+        INSERT INTO auditoria_produto(id_produto, acao, desenvolvedor_id, categoria_id, nome_produto, descricao, preco, tipo, status, data_publicacao, data_atualizacao)
+        VALUES (NEW.id_produto, 'UPDATE', NEW.desenvolvedor_id, NEW.categoria_id, NEW.nome_produto, NEW.descricao, NEW.preco, NEW.tipo, NEW.status, NEW.data_publicacao, NEW.data_atualizacao);
+        RETURN NEW;
+    ELSIF (TG_OP = 'DELETE') THEN
+        INSERT INTO auditoria_produto(id_produto, acao, desenvolvedor_id, categoria_id, nome_produto, descricao, preco, tipo, status, data_publicacao, data_atualizacao)
+        VALUES (OLD.id_produto, 'DELETE', OLD.desenvolvedor_id, OLD.categoria_id, OLD.nome_produto, OLD.descricao, OLD.preco, OLD.tipo, OLD.status, OLD.data_publicacao, OLD.data_atualizacao);
+        RETURN OLD;
+    END IF;
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_auditoria_produto
+AFTER INSERT OR UPDATE OR DELETE ON produto
+FOR EACH ROW
+EXECUTE FUNCTION log_auditoria_produto();
+
+-- Trigger para preenchimento da tabela de auditoria referente à tabela versão
+CREATE OR REPLACE FUNCTION log_auditoria_versao() RETURNS TRIGGER AS $$
+BEGIN
+    IF (TG_OP = 'INSERT') THEN
+        INSERT INTO auditoria_versao(id_versao, acao, produto_id, num_versao, data_lancamento)
+        VALUES (NEW.id_versao, 'INSERT', NEW.produto_id, NEW.num_versao, NEW.data_lancamento);
+        RETURN NEW;
+    ELSIF (TG_OP = 'UPDATE') THEN
+        INSERT INTO auditoria_versao(id_versao, acao, produto_id, num_versao, data_lancamento)
+        VALUES (NEW.id_versao, 'UPDATE', NEW.produto_id, NEW.num_versao, NEW.data_lancamento);
+        RETURN NEW;
+    ELSIF (TG_OP = 'DELETE') THEN
+        INSERT INTO auditoria_versao(id_versao, acao, produto_id, num_versao, data_lancamento)
+        VALUES (OLD.id_versao, 'DELETE', OLD.produto_id, OLD.num_versao, OLD.data_lancamento);
+        RETURN OLD;
+    END IF;
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_auditoria_versao
+AFTER INSERT OR UPDATE OR DELETE ON versao
+FOR EACH ROW
+EXECUTE FUNCTION log_auditoria_versao();
+
+-- Trigger para preenchimento da tabela de auditoria referente à tabela suporte
+CREATE OR REPLACE FUNCTION log_auditoria_suporte() RETURNS TRIGGER AS $$
+BEGIN
+    IF (TG_OP = 'INSERT') THEN
+        INSERT INTO auditoria_suporte(id_suporte, acao, usuario_id, produto_id, versao_id, tipo, descricao, data_suporte, status)
+        VALUES (NEW.id_suporte, 'INSERT', NEW.usuario_id, NEW.produto_id, NEW.versao_id, NEW.tipo, NEW.descricao, NEW.data_suporte, NEW.status);
+        RETURN NEW;
+    ELSIF (TG_OP = 'UPDATE') THEN
+        INSERT INTO auditoria_suporte(id_suporte, acao, usuario_id, produto_id, versao_id, tipo, descricao, data_suporte, status)
+        VALUES (NEW.id_suporte, 'UPDATE', NEW.usuario_id, NEW.produto_id, NEW.versao_id, NEW.tipo, NEW.descricao, NEW.data_suporte, NEW.status);
+        RETURN NEW;
+    ELSIF (TG_OP = 'DELETE') THEN
+        INSERT INTO auditoria_suporte(id_suporte, acao, usuario_id, produto_id, versao_id, tipo, descricao, data_suporte, status)
+        VALUES (OLD.id_suporte, 'DELETE', OLD.usuario_id, OLD.produto_id, OLD.versao_id, OLD.tipo, OLD.descricao, OLD.data_suporte, OLD.status);
+        RETURN OLD;
+    END IF;
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_auditoria_suporte
+AFTER INSERT OR UPDATE OR DELETE ON suporte
+FOR EACH ROW
+EXECUTE FUNCTION log_auditoria_suporte();
+
+-- Trigger para preenchimento da tabela de auditoria referente à tabela versão
+CREATE OR REPLACE FUNCTION log_auditoria_avaliacao() RETURNS TRIGGER AS $$
+    BEGIN
+        IF (TG_OP = 'INSERT') THEN
+            INSERT INTO auditoria_avaliacao(id_avaliacao, acao, usuario_id, versao_id, nota, data_avaliacao)
+            VALUES (NEW.id_avaliacao, 'INSERT', NEW.usuario_id, NEW.versao_id, NEW.nota, NEW.data_avaliacao);
+            RETURN NEW;
+        ELSIF (TG_OP = 'UPDATE') THEN
+            INSERT INTO auditoria_avaliacao(id_avaliacao, acao, usuario_id, versao_id, nota, data_avaliacao)
+            VALUES (NEW.id_avaliacao, 'UPDATE', NEW.usuario_id, NEW.versao_id, NEW.nota, NEW.data_avaliacao);
+            RETURN NEW;
+        ELSIF (TG_OP = 'DELETE') THEN
+            INSERT INTO auditoria_avaliacao(id_avaliacao, acao, usuario_id, versao_id, nota, data_avaliacao)
+            VALUES (OLD.id_avaliacao, 'DELETE', OLD.usuario_id, OLD.versao_id, OLD.nota, OLD.data_avaliacao);
+            RETURN OLD;
+        END IF;
+        RETURN NULL;
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_auditoria_avaliacao
+AFTER INSERT OR UPDATE OR DELETE ON avaliacao
+FOR EACH ROW
+EXECUTE FUNCTION log_auditoria_avaliacao();
+
+-- Trigger para preenchimento da tabela de auditoria referente à tabela versão
+CREATE OR REPLACE FUNCTION log_auditoria_parcela() RETURNS TRIGGER AS $$
+    BEGIN
+        IF (TG_OP = 'INSERT') THEN
+            INSERT INTO auditoria_parcela(id_parcela, acao, assinatura_id, valor, data_vencimento, data_pagamento, status)
+            VALUES (NEW.id_parcela, 'INSERT', NEW.assinatura_id, NEW.valor, NEW.data_vencimento, NEW.data_pagamento, NEW.status);
+            RETURN NEW;
+        ELSIF (TG_OP = 'UPDATE') THEN
+            INSERT INTO auditoria_parcela(id_parcela, acao, assinatura_id, valor, data_vencimento, data_pagamento, status)
+            VALUES (NEW.id_parcela, 'UPDATE', NEW.assinatura_id, NEW.valor, NEW.data_vencimento, NEW.data_pagamento, NEW.status);
+            RETURN NEW;
+        ELSIF (TG_OP = 'DELETE') THEN
+            INSERT INTO auditoria_parcela(id_parcela, acao, assinatura_id, valor, data_vencimento, data_pagamento, status)
+            VALUES (OLD.id_parcela, 'DELETE', OLD.assinatura_id, OLD.valor, OLD.data_vencimento, OLD.data_pagamento, OLD.status);
+            RETURN OLD;
+        END IF;
+        RETURN NULL;
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_auditoria_parcela
+AFTER INSERT OR UPDATE OR DELETE ON parcela
+FOR EACH ROW
+EXECUTE FUNCTION log_auditoria_parcela();

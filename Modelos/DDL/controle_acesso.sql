3f3ac42754f3313fa33656ca2013c237e2f7ac2e
Joseph-DD2 <catce.2024111tads0038@aluno.ifpi.edu.br>
2025-07-05 09:32:42 -0300

    Create controle_acesso.sql

---
+++ Modelos/DDL/controle_acesso.sql
@@ -0,0 +1,11 @@
+-- Usuário comum
+CREATE ROLE role_name WITH LOGIN PASSWORD 'password'
+
+-- Cliente
+CREATE ROLE role_name WITH LOGIN PASSWORD 'password'
+
+-- Desenvolvedor
+CREATE ROLE role_name WITH LOGIN PASSWORD 'password'
+
+-- Admin do marketplace
+CREATE ROLE role_name WITH LOGIN PASSWORD 'password'

2d351fc9e64a0c24d481474b7eac2a5df909cc7e
Joseph-DD2 <catce.2024111tads0038@aluno.ifpi.edu.br>
2025-07-05 15:33:35 -0300

    Tabelas de auditoria

--- Auditoria/tabelas/auditoria.sql
+++ Auditoria/tabelas/auditoria.sql
@@ -13,3 +13,26 @@ CREATE TABLE auditoria_assinatura (
     status STATUS_ASSINATURA,
     data_evento TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+-- Auditoria para a tabela de usuário
+CREATE TABLE auditoria_usuario (
+    id_auditoria SERIAL PRIMARY KEY,
+    id_usuario INT,
+    acao VARCHAR(10), -- 'INSERT', 'UPDATE', 'DELETE'
+    nome_usuario VARCHAR(255),
+    email VARCHAR(255),
+    tipo_usuario TIPOS_USUARIOS,
+    data_registro DATE,
+    data_acao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Auditoria para a tabela de desenvolvedor
+CREATE TABLE auditoria_desenvolvedor (
+    id_auditoria SERIAL PRIMARY KEY,
+    id_desenvolvedor INT,
+    acao VARCHAR(10),
+    nome_dev VARCHAR(255),
+    email_dev VARCHAR(255),
+    data_cadastro DATE,
+    data_acao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
--- Auditoria/triggers_and_functions_auditoria/codes_for_auditoria.sql
+++ Auditoria/triggers_and_functions_auditoria/codes_for_auditoria.sql
@@ -29,3 +29,53 @@ CREATE TRIGGER trg_auditoria_assinatura
 AFTER INSERT OR UPDATE OR DELETE ON assinatura
 FOR EACH ROW
 EXECUTE FUNCTION log_auditoria_assinatura();
+
+-- Trigger para preenchimento da tabela de auditoria referente a tabela assinatura
+CREATE OR REPLACE FUNCTION log_auditoria_usuario() RETURNS TRIGGER AS $$
+    BEGIN
+        IF (TG_OP = 'INSERT') THEN
+            INSERT INTO auditoria_usuario(id_usuario, acao, nome_usuario, email, tipo_usuario, data_registro)
+            VALUES (NEW.id_usuario, 'INSERT', NEW.nome_usuario, NEW.email, NEW.tipo_usuario, NEW.data_registro);
+            RETURN NEW;
+        ELSIF (TG_OP = 'UPDATE') THEN
+            INSERT INTO auditoria_usuario(id_usuario, acao, nome_usuario, email, tipo_usuario, data_registro)
+            VALUES (NEW.id_usuario, 'UPDATE', NEW.nome_usuario, NEW.email, NEW.tipo_usuario, NEW.data_registro);
+            RETURN NEW;
+        ELSIF (TG_OP = 'DELETE') THEN
+            INSERT INTO auditoria_usuario(id_usuario, acao, nome_usuario, email, tipo_usuario, data_registro)
+            VALUES (OLD.id_usuario, 'DELETE', OLD.nome_usuario, OLD.email, OLD.tipo_usuario, OLD.data_registro);
+            RETURN OLD;
+        END IF;
+        RETURN NULL;
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_auditoria_usuario
+AFTER INSERT OR UPDATE OR DELETE ON usuario
+FOR EACH ROW
+EXECUTE FUNCTION log_auditoria_usuario();
+
+-- Trigger para preenchimento da tabela de auditoria referente a tabela desenvolvimento
+CREATE OR REPLACE FUNCTION log_auditoria_desenvolvedor() RETURNS TRIGGER AS $$
+BEGIN
+    IF (TG_OP = 'INSERT') THEN
+        INSERT INTO auditoria_desenvolvedor(id_desenvolvedor, acao, nome_dev, email_dev, data_cadastro)
+        VALUES (NEW.id_desenvolvedor, 'INSERT', NEW.nome_dev, NEW.email_dev, NEW.data_cadastro);
+        RETURN NEW;
+    ELSIF (TG_OP = 'UPDATE') THEN
+        INSERT INTO auditoria_desenvolvedor(id_desenvolvedor, acao, nome_dev, email_dev, data_cadastro)
+        VALUES (NEW.id_desenvolvedor, 'UPDATE', NEW.nome_dev, NEW.email_dev, NEW.data_cadastro);
+        RETURN NEW;
+    ELSIF (TG_OP = 'DELETE') THEN
+        INSERT INTO auditoria_desenvolvedor(id_desenvolvedor, acao, nome_dev, email_dev, data_cadastro)
+        VALUES (OLD.id_desenvolvedor, 'DELETE', OLD.nome_dev, OLD.email_dev, OLD.data_cadastro);
+        RETURN OLD;
+    END IF;
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_auditoria_desenvolvedor
+AFTER INSERT OR UPDATE OR DELETE ON desenvolvedor
+FOR EACH ROW
+EXECUTE FUNCTION log_auditoria_desenvolvedor();
